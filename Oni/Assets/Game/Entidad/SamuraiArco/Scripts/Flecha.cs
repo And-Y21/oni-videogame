5b34b822834bccf292d72bc83178d792acba0a80
FernandoRmz13 <luisrmz1307@gmail.com>
2026-03-15 02:48:47 -0600

    Samurai con arco, animaciones y scripts

---
+++ Oni/Assets/Game/Entidad/SamuraiArco/Scripts/Flecha.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+public class Flecha : MonoBehaviour
+{
+    [SerializeField] private float velocidad;
+    [SerializeField] private float daño;
+
+    private int direccion = 1;
+
+    private void Update()
+    {
+        transform.Translate(Vector2.right * direccion * velocidad * Time.deltaTime);
+    }
+
+    public void SetDireccion(int dir)
+    {
+        direccion = dir;
+
+        Vector3 escala = transform.localScale;
+        escala.x = Mathf.Abs(escala.x) * dir;
+        transform.localScale = escala;
+    }
+}
