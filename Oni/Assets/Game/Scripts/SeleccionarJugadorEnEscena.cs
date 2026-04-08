be2368420981a52bd68d0108d3215a90c093bf5c
FernandoRmz13 <luisrmz1307@gmail.com>
2026-04-08 03:26:08 -0600

    Cambios en escena antes de merge

---
+++ Oni/Assets/Game/Scripts/SeleccionarJugadorEnEscena.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+using Unity.Cinemachine;
+
+public class SeleccionarJugadorEnEscena : MonoBehaviour
+{
+    public GameObject player1;
+    public GameObject player2;
+    public CinemachineCamera virtualCamera;
+
+    void Start()
+    {
+        int selectedCharacter = PlayerPrefs.GetInt("SelectedCharacter", 0);
+
+        player1.SetActive(false);
+        player2.SetActive(false);
+
+        GameObject jugadorActivo;
+
+        if (selectedCharacter == 0)
+        {
+            jugadorActivo = player1;
+        }
+        else
+        {
+            jugadorActivo = player2;
+        }
+
+        jugadorActivo.SetActive(true);
+
+        // 🔥 AQUÍ CAMBIAS EL TARGET DE LA CÁMARA
+        if (virtualCamera != null)
+        {
+            virtualCamera.Target.TrackingTarget = jugadorActivo.transform;
+        }
+
+        // Activar input
+        PlayerInput input = jugadorActivo.GetComponent<PlayerInput>();
+        if (input != null)
+        {
+            input.ActivateInput();
+        }
+    }
+}
